c5b97cb9513a24f1c0d18f72dbf6177e6c4ace01
HecticSwede <hectorastr@gmail.com>
2024-03-23 10:42:35 +0100

    Morning Progress

---
+++ Assets/Scripts/Global Strings.cs
@@ -0,0 +1,28 @@
+
+public static class GlobalStrings
+{
+    public const string START_NPC = "";
+
+    public const string NPC_1_TRUE = "";
+    public const string NPC_2_TRUE = "";
+    public const string NPC_3_TRUE = "";
+    public const string NPC_4_TRUE = "";
+    public const string NPC_5_TRUE = "";
+
+    public const string NPC_1_FALSE = "";
+    public const string NPC_2_FALSE = "";
+    public const string NPC_3_FALSE = "";
+    public const string NPC_4_FALSE = "";
+    public const string NPC_5_FALSE = "";
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Assets/Scripts/LightObjective.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightObjective : MonoBehaviour
+{
+    public bool Activated { get; set; } = false;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
